c77573aa4f7c98521349208a68b1b98fe9ffe98c
MagicSaucer2636 <magicsaucer2636@gmail.com>
2024-05-07 00:19:50 +0200

    FindYourMovie V1.0

--- movie.js
+++ movie.js
@@ -6,107 +6,99 @@ const infos = document.querySelector('.info-container');
 const apiKey = "86a1d264aff49d58b5d19eac7b08daa0";
 
 // Variables
-let displayedMovies = [];
 let crash = 0;
 
 
 // Functions
-function loadMovie(id=false) {
-    const data = getRandomMovie(id).then(data => {
-      if (data && !displayedMovies.includes(data.id)) {
-        const movie = document.createElement('a');
-        movie.href = `./movie.html?id=${data.id}`;
-  
-        let heures = Math.floor(data.time / 60);
-        let minutesRestantes = data.time % 60;
-        let heuresFormatées = ('0' + heures).slice(-2);
-        let minutesFormatées = ('0' + minutesRestantes).slice(-2);
-  
-        movie.innerHTML = `
-          <div class="poster">
-            <img src="https://image.tmdb.org/t/p/w500/${data.imageUrl}" alt="${data.title}">
-            <p>${heuresFormatées + ':' + minutesFormatées}:00</p>
-          </div>
-          <div class="movie-infos">
-            <h1>${data.title}</h1>
-            <p>${data.releaseDate.substring(0, 4)} • ${data.director}</p>
-          </div>
-        `;
-  
-        movie.classList.add('film');
-        container.appendChild(movie);
-        displayedMovies.push(data.id);
-  
-        resizePosters();
-      } else {
-        crash += 1;
-        if (crash > 100) {
-          console.log('Erreur lors du chargement des films. Veuillez réessayer.');
-          isLoading = true;
-          return;
-        }
-        loadMovie();
+function loadMovie(id) {
+  getRandomMovie(id).then(data => {
+    if (data) {
+      const movie = document.createElement('a');
+      movie.href = `./movie?id=${data.id}`;
+
+      let heures = Math.floor(data.time / 60);
+      let minutesRestantes = data.time % 60;
+      let heuresFormatées = ('0' + heures).slice(-2);
+      let minutesFormatées = ('0' + minutesRestantes).slice(-2);
+
+      movie.innerHTML = `
+        <div class="poster">
+          <img src="https://image.tmdb.org/t/p/w500/${data.imageUrl}" alt="${data.title}">
+          <p>${heuresFormatées + ':' + minutesFormatées}:00</p>
+        </div>
+        <div class="movie-infos">
+          <h1>${data.title}</h1>
+          <p>${data.releaseDate.substring(0, 4)} • ${data.director}</p>
+        </div>
+      `;
+
+      movie.classList.add('film');
+      container.appendChild(movie);
+
+      resizePosters();
+    } else {
+      crash += 1;
+      if (crash > 100) {
+        console.log('Erreur lors du chargement des films. Veuillez réessayer.');
+        isLoading = true;
+        return;
       }
-    });
+    }
+  });
 }
 
 async function getRandomMovie(id) {
+  try {
+    let movieDetails;
+    let credits;
+    movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=fr-FR`);
+    credits = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`);
+
+    if (!movieDetails.data.poster_path || movieDetails.data.title === movieDetails.data.original_title || movieDetails.data.adult || movieDetails.data.runtime < 60) {
+      return null;
+    }
+
+    let director = 'Réaliseur inconnu';
+    for (let i = 0; i < credits.data.crew.length; i++) {
+      if (credits.data.crew[i].job === 'Director') {
+        director = credits.data.crew[i].name;
+        break;
+      }
+    }
+
+    let trailer = null;
     try {
-      let movieDetails;
-      let credits;
-      if(id === false) {
-        let randomPage;
-        let url;
-        if( filters === "all") {
-          randomPage = Math.floor(Math.random() * 500) + 1;
-          url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomPage}`;
-        }
-        
-        if (filters !== "all") {
-          randomPage = Math.floor(Math.random() * 300) + 1;
-          url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomPage}&with_genres=${correspondances[filters]}`;
-        }
-        const response = await axios.get(url);
-        const randomMovie = response.data.results[Math.floor(Math.random() * 20)];
-  
-        movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${randomMovie.id}?api_key=${apiKey}&language=fr-FR`);
-        credits = await axios.get(`https://api.themoviedb.org/3/movie/${randomMovie.id}/credits?api_key=${apiKey}`);
-      } else {
-        movieDetails = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=fr-FR`);
-        credits = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`);
-      }
-  
-  
-      if (!movieDetails.data.poster_path || movieDetails.data.title === movieDetails.data.original_title || movieDetails.data.adult || movieDetails.data.runtime < 60) {
-        return getRandomMovie(id);
-      }
-  
-      let director = 'Réaliseur inconnu';
-      for (let i = 0; i < credits.data.crew.length; i++) {
-        if (credits.data.crew[i].job === 'Director') {
-          director = credits.data.crew[i].name;
+      const videos = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=fr-FR`);
+      for (let i = 0; i < videos.data.results.length; i++) {
+        if (videos.data.results[i].type === 'Trailer') {
+          trailer = videos.data.results[i].key;
           break;
         }
       }
-  
-      return {
-        id: movieDetails.data.id,
-        title: movieDetails.data.title,
-        releaseDate: movieDetails.data.release_date,
-        imageUrl: movieDetails.data.poster_path,
-        time: movieDetails.data.runtime,
-        director: director
-      };
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
+    } catch {}
+    console.log(trailer);
+
+    // ajouter trailer si diferent de null
+    return {
+      id: movieDetails.data.id,
+      title: movieDetails.data.title,
+      releaseDate: movieDetails.data.release_date,
+      imageUrl: movieDetails.data.poster_path,
+      time: movieDetails.data.runtime,
+      director: director,
+      genres: movieDetails.data.genres,
+      trailer: trailer,
+      sinopsis: movieDetails.data.overview
+    };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 async function loadSimilarMovies(id) {
     try {
       const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}&language=fr-FR&page=1`);
-      console.log(similarMovies.data.results);
       similarMovies.data.results.map(movie => {
         loadMovie(movie.id);
       });
@@ -120,7 +112,14 @@ function resizePosters() {
     posters.forEach(poster => {
       poster.style.height = poster.offsetWidth * 1.5 + 'px';
     });
-  }
+}
+
+function resizeIframe() {
+  const iframes = document.querySelectorAll('.trailer');
+  iframes.forEach(iframe => {
+    iframe.style.height = iframe.offsetWidth * 0.5 + 'px';
+  });
+}
 
 
 // Events
@@ -132,6 +131,7 @@ document.querySelectorAll('.menu-button').forEach(button => {
 
 window.addEventListener('resize', () => {
     resizePosters();
+    resizeIframe();
     if (window.innerWidth > 768) {
       document.querySelector('.menu-mobile').classList.remove('visible');
     }
@@ -149,4 +149,59 @@ if (!id) {
     const movie = await getRandomMovie(id);
     document.title = "FindYourMovie » " + movie.title;
     loadSimilarMovies(id);
+    infos.innerHTML = `
+    <div class='no-iframe'>
+      <section class="i-poster">
+        <img src="https://image.tmdb.org/t/p/w500/${movie.imageUrl}" alt="${movie.title}">
+      </section>
+      <div class="infos">
+        <section class="title">
+          <h1><strong>${movie.title}</strong> (${movie.releaseDate.substring(0, 4)})</h1>
+        </section>
+        <div class="infos-details">
+          <section class="date-director">
+            <p>${movie.releaseDate}  •  ${movie.director}</p>
+          </section>
+          <section class="time">
+            <p>${Math.floor(movie.time / 60)}h${movie.time % 60}</p>
+          </section>
+          <section class="genres">
+            <p>${movie.genres.map(genre => genre.name).join(' • ')}</p>
+          </section>
+        </div>
+      </div>
+    </div>
+    `;
+    if (movie.trailer) {
+      document.querySelector('.infos').innerHTML += `
+        <section class="trailer t-desk">
+        
+          <iframe src="https://www.youtube.com/embed/${movie.trailer}" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+      `;
+      infos.innerHTML += `
+        <section class="trailer t-mobile">
+          <iframe  src="https://www.youtube.com/embed/${movie.trailer}" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+      `;
+      resizeIframe();
+    } else {
+      // Add sinopsis
+      document.querySelector('.infos').innerHTML += `
+        <section class="sinopsis t-desk">
+          <p>${movie.sinopsis}</p>
+        </section>
+        <section class="no-trailer t-desk">
+          <p>Aucune bande-annonce disponible pour ce film.</p>
+        </section>
+      `;
+      infos.innerHTML += `
+        <section class="sinopsis t-mobile">
+          <p>${movie.sinopsis}</p>
+        </section>
+        <section class="no-trailer t-mobile">
+          <p>Aucune bande-annonce disponible pour ce film.</p>
+        </section>
+      `;
+    }
 })();
--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const apiKey = "86a1d264aff49d58b5d19eac7b08daa0";
 
 // Variables
 let isLoading = false;
+let search_mode = false;
 let displayedMovies = [];
 let filters = "all";
 let crash = 0;
-let search_mode = false;
 
+// Correspondances
 const correspondances = {
   "action": 28,
   "aventure": 12,
@@ -43,21 +44,26 @@ function resizePosters() {
 }
 
 function loadMovies() {
+  if(isLoading || search_mode) return;
   isLoading = true;
   for (let i = 0; i < 24; i++) {
     loadMovie();
   }
-  isLoading = false;
+  setTimeout(() => {
+    isLoading = false;
+  }, 1000);
 }
 
 function loadMore() {
-  if (isLoading) return;
+  if(isLoading || search_mode) return;
   isLoading = true;
   crash = 0;
   for (let i = 0; i < 24; i++) {
     loadMovie();
   }
-  isLoading = false;
+  setTimeout(() => {
+    isLoading = false;
+  }, 1000);
 }
 
 function filtersChange() {
@@ -67,54 +73,96 @@ function filtersChange() {
   for (let i = 0; i < 24; i++) {
     loadMovie();
   }
-  isLoading = false;
+  setTimeout(() => {
+    isLoading = false;
+  }, 1000);
 }
 
 function refresh() {
+  isLoading = true;
   container.innerHTML = "";
   const loader = document.querySelector('.loader');
   loader.style.display = 'flex';
   displayedMovies.map(movie => {
-    loadMovies(movie);
+    loadMovie(movie);
   });
-  search_mode = false;
+  setTimeout(() => {
+    search_mode = false;
+    isLoading = false;
+  }, 1000);
 }
 
 function loadMovie(id=false) {
-  const data = getRandomMovie(id).then(data => {
-    if (data && !displayedMovies.includes(data.id)) {
-      const movie = document.createElement('a');
-      movie.href = `./movie?id=${data.id}`;
-
-      let heures = Math.floor(data.time / 60);
-      let minutesRestantes = data.time % 60;
-      let heuresFormatées = ('0' + heures).slice(-2);
-      let minutesFormatées = ('0' + minutesRestantes).slice(-2);
-
-      movie.innerHTML = `
-        <div class="poster">
-          <img src="https://image.tmdb.org/t/p/w500/${data.imageUrl}" alt="${data.title}">
-          <p>${heuresFormatées + ':' + minutesFormatées}:00</p>
-        </div>
-        <div class="movie-infos">
-          <h1>${data.title}</h1>
-          <p>${data.releaseDate.substring(0, 4)} • ${data.director}</p>
-        </div>
-      `;
-
-      movie.classList.add('film');
-      container.appendChild(movie);
-      displayedMovies.push(data.id);
-
-      resizePosters();
-    } else {
-      crash += 1;
-      if (crash > 100) {
-        console.log('Erreur lors du chargement des films. Veuillez réessayer.');
-        isLoading = true;
-        return;
+  getRandomMovie(id).then(data => {
+    if(id === false) {
+      if (data && !displayedMovies.includes(data.id)) {
+        const movie = document.createElement('a');
+        movie.href = `./movie?id=${data.id}`;
+  
+        let heures = Math.floor(data.time / 60);
+        let minutesRestantes = data.time % 60;
+        let heuresFormatées = ('0' + heures).slice(-2);
+        let minutesFormatées = ('0' + minutesRestantes).slice(-2);
+  
+        movie.innerHTML = `
+          <div class="poster">
+            <img src="https://image.tmdb.org/t/p/w500/${data.imageUrl}" alt="${data.title}">
+            <p>${heuresFormatées + ':' + minutesFormatées}:00</p>
+          </div>
+          <div class="movie-infos">
+            <h1>${data.title}</h1>
+            <p>${data.releaseDate.substring(0, 4)} • ${data.director}</p>
+          </div>
+        `;
+  
+        movie.classList.add('film');
+        container.appendChild(movie);
+        displayedMovies.push(data.id);
+  
+        resizePosters();
+      } else {
+        crash += 1;
+        if (crash > 100) {
+          console.log('Erreur lors du chargement des films. Veuillez réessayer.');
+          isLoading = true;
+          return;
+        }
+        loadMovie();
       }
-      loadMovie();
+    } else {
+      if (data) {
+        const movie = document.createElement('a');
+        movie.href = `./movie?id=${data.id}`;
+  
+        let heures = Math.floor(data.time / 60);
+        let minutesRestantes = data.time % 60;
+        let heuresFormatées = ('0' + heures).slice(-2);
+        let minutesFormatées = ('0' + minutesRestantes).slice(-2);
+  
+        movie.innerHTML = `
+          <div class="poster">
+            <img src="https://image.tmdb.org/t/p/w500/${data.imageUrl}" alt="${data.title}">
+            <p>${heuresFormatées + ':' + minutesFormatées}:00</p>
+          </div>
+          <div class="movie-infos">
+            <h1>${data.title}</h1>
+            <p>${data.releaseDate.substring(0, 4)} • ${data.director}</p>
+          </div>
+        `;
+  
+        movie.classList.add('film');
+        container.appendChild(movie);
+  
+        resizePosters();
+      } else {
+        crash += 1;
+        if (crash > 100) {
+          console.log('Erreur lors du chargement des films. Veuillez réessayer.');
+          isLoading = true;
+          return;
+        }
+        loadMovie();
+      }
     }
   });
 }
@@ -128,7 +176,7 @@ async function getRandomMovie(id) {
       let url;
       if( filters === "all") {
         randomPage = Math.floor(Math.random() * 500) + 1;
-        url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomPage}`;
+        url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=true&page=${randomPage}`;
       }
       
       if (filters !== "all") {
@@ -172,30 +220,24 @@ async function getRandomMovie(id) {
   }
 }
 
-function searchInDB(text) {
+async function searchInDB(text) {
   crash = 0;
-  container.innerHTML = "";
-  try {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${encodeURIComponent(text)}&page=1&include_adult=false`).then(response => {
-    console.log(response.data.results);  
+  await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${encodeURIComponent(text)}&page=1&include_adult=false`).then(response => {
     if(response.data.results.length === 0) {
-        const noResult = document.createElement('div');
-        noResult.classList.add('no-result');
-        noResult.innerHTML = `
-          <h1>Aucun résultat trouvé</h1>
-        `;
-        container.appendChild(noResult);
-      } else {
-        response.data.results.map(movie => {
-          loadMovie(movie.id);
-        });
-      }
-    }).catch(error => {
-      console.error(error);
-    });
-  } catch (error) {
+      const noResult = document.createElement('div');
+      noResult.classList.add('no-result');
+      noResult.innerHTML = `
+        <h1>Aucun résultat trouvé</h1>
+      `;
+      container.appendChild(noResult);
+    } else {
+      response.data.results.map(movie => {
+        loadMovie(movie.id);
+      });
+    }
+  }).catch(error => {
     console.error(error);
-  }
+  });
 }
 
 // Events
@@ -221,7 +263,7 @@ document.querySelectorAll('.menu-button').forEach(button => {
 
 document.querySelectorAll('button').forEach(button => {
   button.addEventListener('click', () => {
-    if(search_mode) return;
+    if(search_mode || isLoading) return;
     document.querySelector('.' + filters).classList.remove('active');
     filters = button.classList.item(0);
     button.classList.add('active');
@@ -231,21 +273,14 @@ document.querySelectorAll('button').forEach(button => {
 
 document.querySelector('.search input').addEventListener('input', (e) => {
   const search = e.target.value;
-  if(search === "") {
-    if(search_mode) {
-      refresh();
-    }
+  if(search === "" && search_mode) {
+    refresh();
   } else {
-    if(!search_mode) {
-      search_mode = true;
-      container.innerHTML = "";
-      const loader = document.querySelector('.loader');
-      loader.style.display = 'none';
-
-      searchInDB(search);
-    } else {
-      searchInDB(search);
-    }
+    search_mode = true;
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'none';
+    container.innerHTML = "";
+    searchInDB(search);
   }
 });
 
